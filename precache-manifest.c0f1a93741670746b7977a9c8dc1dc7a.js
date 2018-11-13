75241545e7a64515c3a94b184f03f2a9e536328e
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-13 15:56:05 -0500

    Updates

---
+++ precache-manifest.c0f1a93741670746b7977a9c8dc1dc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "82fc413f8c386c6922d2",
+    "url": "/react-tic-tac-toe/static/js/main.82fc413f.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/react-tic-tac-toe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "82fc413f8c386c6922d2",
+    "url": "/react-tic-tac-toe/static/css/main.3ee26037.chunk.css"
+  },
+  {
+    "revision": "a593989e93d56df952efd585b1a15db4",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
